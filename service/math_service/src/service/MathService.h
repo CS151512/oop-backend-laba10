c1109848f3543c85915312496fc5bcdaf6bfa423
Finnik <73883383+golkity@users.noreply.github.com>
2026-04-15 18:04:20 +0300

    Update(service layer): implement function for new opperator with complex and fcuntion for format in decard to pola system coords

--- service/math_service/src/service/MathService.h
+++ service/math_service/src/service/MathService.h
@@ -12,6 +12,13 @@ public:
     explicit MathService(IMathRepository& repository);
 
     std::string addComplex(const my_complex& a, const my_complex& b);
+    std::string subComplex(const my_complex& a, const my_complex& b);
+    std::string mulComplex(const my_complex& a, const my_complex& b);
+    std::string divComplex(const my_complex& a, const my_complex& b);
+    std::string eqComplex(const my_complex& a, const my_complex& b);
+    std::string neqComplex(const my_complex& a, const my_complex& b);
+    std::string toPolarComplex(const my_complex& c);
+
     double getTriangleArea(double a, double b, double c);
     std::string addRationals(const Rational& r1, const Rational& r2);
     double getRingArea(double outer, double inner);
